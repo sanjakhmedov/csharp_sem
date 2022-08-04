9d54a4379776993403aa71e5875537ac9968e253
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-04 13:51:45 +0500

    task 2 done

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -11,15 +11,35 @@
 // int square = num * num;
 // Console.WriteLine($"Square of the {num} is {square}");
 
-Console.Write("Enter the first integer number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second integer number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the first integer number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second integer number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 == num2*num2) {
-    Console.WriteLine($"{num1} = square of {num2}");
+// if (num1 == num2*num2) {
+//     Console.WriteLine($"{num1} = square of {num2}");
+// } else {
+//     Console.WriteLine($"{num1} is not the square of {num2}");
+// };
+
+
+Console.WriteLine("Enter the enteger number from 1 to 7 inclusive: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) {
+    Console.WriteLine($"{number} = Monday");
+} else if (number == 2) {
+    Console.WriteLine($"{number} = Tuesday");
+} else if (number == 3) {
+    Console.WriteLine($"{number} = Wednesday");
+} else if (number == 4) {
+    Console.WriteLine($"{number} = Thursday");
+} else if (number == 5) {
+    Console.WriteLine($"{number} = Friday");
+} else if (number == 6) {
+    Console.WriteLine($"{number} = Saturday");
+} else if (number == 7) {
+    Console.WriteLine($"{number} = Sunday");
 } else {
-    Console.WriteLine($"{num1} is not the square of {num2}");
+    Console.WriteLine("You entered wrong number");
 };
-
-
