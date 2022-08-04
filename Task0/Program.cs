c8ac1604b3d80682c76398e1d900d3c2d1d49d33
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-04 13:43:30 +0500

    task 1 is done

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+// Console.Write("Enter the integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int square = num * num;
+// Console.WriteLine($"Square of the {num} is {square}");
+
+Console.Write("Enter the first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2*num2) {
+    Console.WriteLine($"{num1} = square of {num2}");
+} else {
+    Console.WriteLine($"{num1} is not the square of {num2}");
+};
+
+
