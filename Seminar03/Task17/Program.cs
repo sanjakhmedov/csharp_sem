e42ccab7334f92c6a9f7d1a0df860e530c9769d7
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-13 23:53:07 +0500

    Task 17 is process

--- Seminar03/Task17/Program.cs
+++ Seminar03/Task17/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter coordinate for X: ");
+int x = Convert.ToInt32(ReadLine());
+Console.Write("Enter coordinate for Y: ");
+int y = Convert.ToInt32(ReadLine());
+
+int XY(int x1, int y1)
+{
+    if (x1 > 0 && y1 > 0) {
+        return 1;
+    } 
+    else if (x1 < 0 && y1 > 0) 
+    {
+        return 2;
+    }
+    else if (x1 < 0 && y1 < 0) 
+    {
+        return 3;
+    }
+    else // if (x1 > 0 && y1 < 0) 
+    {
+        return 4;
+    }
+}
+
+XY(x, y);
