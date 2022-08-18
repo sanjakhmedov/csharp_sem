9a44660bd46dd88b2c9d4ba21f612827ae20844b
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-18 10:25:39 +0500

    seminar examples

---
+++ Seminar04/Task24/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+for (int i = 1; i <= a; i++)
+{
+    result += i;
+}
+Console.WriteLine(result);
---
+++ Seminar04/Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Enter the integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int NumCount(int num_m)
+{
+    int counter = 1;
+    while (num_m >= 10)
+    {
+        num_m = num_m / 10;
+        counter++;
+        
+    }
+    return counter;
+}
+int res = NumCount(num);
+Console.WriteLine(res);
