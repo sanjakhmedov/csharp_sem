b691fa578a9b926b1e28549188cc4d1ab0ffdb70
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-18 10:37:09 +0500

    task 28 done

---
+++ Seminar04/Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Enter the integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int n)
+{
+    int res = 1;
+    for (int i = 1; i < n; i++)
+    {
+        res = res + res*i;
+    }
+    return res;
+}
+
+int sum = Factorial(num);
+Console.WriteLine(sum);
