d6c3af06c548f5986be7327b8d3ea57817151257
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-25 20:13:01 +0500

    task 40 is done

--- Seminar06/Task40/Program.cs
+++ Seminar06/Task40/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+//
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон
+
+Console.Write("Enter the number ");
+Console.Write("a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool TriangleSidesCheck(int a_m, int b_m, int c_m)
+{
+    if(a_m + b_m <= c_m || a_m + c_m <= b_m || b_m + c_m <= a_m)
+        return false;
+    else
+        return true;
+}
+
+bool result = TriangleSidesCheck(a, b, c);
+Console.WriteLine(result);
