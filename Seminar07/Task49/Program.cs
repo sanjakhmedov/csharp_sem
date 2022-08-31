def09cd8f486eb086bde6c06b7d4710c689082d3
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-31 20:06:06 +0500

    inclass tasks 49 and 51 are done

---
+++ Seminar07/Task49/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4}, ");
+            else Console.Write($"{matrix[i,j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void SquareEvenIndexElem(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] matr = CreateMatrixRndInt(5, 5, 1, 9);
+PrintMatrix(matr);
+Console.WriteLine();
+SquareEvenIndexElem(matr);
+PrintMatrix(matr);
---
+++ Seminar07/Task51/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matr = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4}, ");
+            else Console.Write($"{matrix[i,j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+    Console.WriteLine();
+}
+
+int MatrDiagonalSum(int[,] matr)
+{
+    int sumOfDiagonal = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == j) sumOfDiagonal += matr[i, j];
+        }
+    }
+    return sumOfDiagonal;
+}
+
+int[,] matrix = CreateMatrixRndInt(5, 5, 1, 9);
+PrintMatrix(matrix);
+int matrDiagonalSum = MatrDiagonalSum(matrix);
+Console.WriteLine("Matrix diagonal sum equals " + matrDiagonalSum);
+
