986c2f4da2dccfa6d21b7fd7a63bd5d856e1d070
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-09-01 12:31:59 +0500

    inclass task 63 is done

---
+++ Seminar09/Task63/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Enter the intiger number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NatularNumber(int num)
+{
+    if(num == 0) return;
+    NatularNumber(num - 1);
+    Console.Write($"{num} ");
+}
+
+NatularNumber(number);
