c43ebacab52ca8888c7d7254623f0b6a4bf417d3
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-09-01 13:00:27 +0500

    task 65 is done

--- Seminar09/Task65/Program.cs
+++ Seminar09/Task65/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+Console.WriteLine("Enter the intiger number: ");
+Console.Write("M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int m, int n)
+{
+    if (m == n) 
+    {
+        Console.Write($"{m} "); 
+        return;
+    }
+    if (m > n)
+    {
+        Console.Write($"{m} ");
+        NaturalNumber(m - 1, n);
+    }
+    else 
+    {
+        NaturalNumber(m, n - 1);
+        Console.Write($"{n} ");
+    }
+}
+
+NaturalNumber(m, n);
