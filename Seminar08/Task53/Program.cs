b8ba1ffe0612d2ed2d4cacded05e4475e928f639
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-09-01 10:35:36 +0500

    inclass task 53 is done

--- Seminar08/Task53/Program.cs
+++ Seminar08/Task53/Program.cs
@@ -16,7 +16,6 @@ int[,] CreateMatrixRndInt(int row, int col, int min, int max)
     return matr;
 }
 
-
 void PrintMatrix(int[,] matr)
 {
 
@@ -26,7 +25,7 @@ void PrintMatrix(int[,] matr)
         for (int j = 0; j < matr.GetLength(1); j++)
         {
 
-            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],4} ");
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],4}, ");
             else Console.Write($"{matr[i, j],4} ");
 
         }
@@ -34,6 +33,23 @@ void PrintMatrix(int[,] matr)
     }
 }
 
+void ChangeFirstToLastRow(int[,] matr)
+{
+    int tempF = 0;
+    int tempL = 0;
+    int row = matr.GetLength(0);
+    int col = matr.GetLength(1);
+    for (int i = 0; i < col; i++)
+    {
+        tempF = matr[0, i];
+        tempL = matr[col - 1, i];
+        matr[0, i] = tempL;
+        matr[col - 1, i] = tempF;
+    }
+}
 
 int[,] matrix = CreateMatrixRndInt(5, 5, 0, 10);
+PrintMatrix(matrix);
+ChangeFirstToLastRow(matrix);
+Console.WriteLine();
 PrintMatrix(matrix);
